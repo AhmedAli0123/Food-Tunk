28eef9721a7295ad8d49f885ae8b72630df784dd
AhmedAli0123 <ahmedsiridab1@gmail.com>
2025-02-01 14:01:32 +0500

    Fix Some Issue

--- my-app/next.config.mjs
+++ my-app/next.config.mjs
@@ -1,8 +1,16 @@
+import 'dotenv/config';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: ['cdn.dummyjson.com',"cdn.sanity.io"], // Add the hostname here
-      },
+  images: {
+    domains: ['cdn.dummyjson.com', 'cdn.sanity.io'], // Add the hostname here
+  },
+  env: {
+    NEXT_PUBLIC_SANITY_API_VERSION: process.env.NEXT_PUBLIC_SANITY_API_VERSION,
+    NEXT_PUBLIC_SANITY_DATASET: process.env.NEXT_PUBLIC_SANITY_DATASET,
+    NEXT_PUBLIC_SANITY_PROJECT_ID: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+    SANITY_API_TOKEN: process.env.SANITY_API_TOKEN,
+  },
 };
 
 export default nextConfig;
--- my-app/src/sanity/lib/client.ts
+++ my-app/src/sanity/lib/client.ts
@@ -1,11 +1,11 @@
 import { createClient } from 'next-sanity'
 
-import { apiVersion, dataset, projectId } from '../env'
+import { apiVersion, dataset, projectId, Apitoken } from '../env'
 
 export const client = createClient({
-  projectId:"wyzsknba",
-  dataset: "production",
-  apiVersion: '2023-03-25',
+  projectId,
+  dataset,
+  apiVersion,
   useCdn: true, // Set to false if statically generating pages, using ISR or tag-based revalidation
-  token: "skLlt1g5MaMSxUu6DQzR0fKEmGjClyt46j3n49Kj2OOuaRXbVU1ssnN1ZleX5dpYy8zHpcns4OF1knUvRZI1uESIHvnfIaZo3qtRQv9uKBZZ64dxFi40dO88UOBPejdCRAhDDiaau4dLCCO6DMsxtO8JVqZOB3CRr0U877Nq35AsgV6SkQPY",
+  token:Apitoken
 })
